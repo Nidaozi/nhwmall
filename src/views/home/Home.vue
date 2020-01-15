c3fa78171cc89e6c17b3050188615f47497aa9a8
倪宏伟的计算机 <3141610964@qq.com>
2020-01-15 19:18:39 +0800

    2020/1/5

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,12 +1,43 @@
 <template>
-  <h2>主页</h2>
+  <div id="home">
+    <nav-bar class="home-navbar">
+      <div slot="center">购物街</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
+import { getHomeMultidata } from 'network/home'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    NavBar
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: []
+    }
+  },
+  created() {
+    getHomeMultidata()
+      .then(result => {
+        console.log(result)
+
+        this.banners = result.data.banner.list
+        this.recommends = result.data.recommend.list
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
 }
 </script>
 
 <style>
+.home-navbar {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
